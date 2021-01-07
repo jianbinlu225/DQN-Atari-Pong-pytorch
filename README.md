90a22de38d266b3310b430e2c36315f791daa4ab
jianbinlu225 <dashuaiguolou@gmail.com>
2021-01-07 20:56:28 +0800

    test gif and video

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # README
 
 Homework
+
+环境：
+- python 3.7.9
+- pytorch 1.7.1
+- numpy 1.19.2
+- tensorboardX 1.2
+- tensorboard 2.4.0
+
+测试：
+
+![测试](./asserts/测试.gif)
+
+运行：
+
+- 训练：`python run.py`
+- 测试：`python test.py`
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import cv2
 import time
 
 from agent import Agent
-# from q_network import QNetwork
+from q_network import QNetwork
 from wrappers import make_env
 
 ENV_NAME='PongNoFrameskip-v4'
@@ -15,11 +15,38 @@ DEVICE='cuda' if torch.cuda.is_available() else 'cpu'
 
 
 
+def load_model(path, input_shape, n_actions):
+    dqn = QNetwork(input_shape,n_actions).to(DEVICE)
+    dqn.load_state_dict(torch.load(path))
+
+    return dqn
+
+
+def play(env, model, device="cpu"):
+    observation = env.reset()
+    total_reward = 0
+    while (True):
+        env.render()
+        # action = env.action_space.sample() # your agent here (this takes random actions)
+
+        observation_a = np.array([observation], copy=False)
+        observation_v = torch.tensor(observation_a).to(device)
+        q_vals_v = model(observation_v)
+        _, act_v = torch.max(q_vals_v, dim=1)
+        action = int(act_v.item())
+
+        observation, reward, done, info = env.step(action)
+
+        total_reward += reward
+
+        if done:
+            return total_reward
+
 if __name__ == '__main__':
 
     env = make_env(ENV_NAME)
 
-    dqn_model = Agent(env.observation_space.shape, env.action_space.n)
+    dqn_model = load_model("./solved_rb_10k.dat", env.observation_space.shape, env.action_space.n)
 
     total_reward = 0
     for i in range(10):
